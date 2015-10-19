e30ce5b6a9858cc25a280c8cb7aab6fc7b627fed
atteheino <atte.heino@gmail.com>
2015-10-19 20:43:03 +0300

    Formatting of texts in ListView

--- app/src/main/java/fi/atteheino/littlehelper/adapter/IBeaconArrayAdapter.java
+++ app/src/main/java/fi/atteheino/littlehelper/adapter/IBeaconArrayAdapter.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import org.altbeacon.beacon.Beacon;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.List;
 
 import fi.atteheino.littlehelper.R;
@@ -17,10 +19,17 @@ import fi.atteheino.littlehelper.R;
  * Created by atte on 16.10.2015.
  */
 public class IBeaconArrayAdapter extends ArrayAdapter {
+    private static final String UNKNOWN_DEVICE = "Unknown Device";
     private List beacons;
     public IBeaconArrayAdapter(Context context, int resource, int textViewResourceId, List objects) {
         super(context, resource, textViewResourceId, objects);
         this.beacons = objects;
+    }
+
+    private static String formatDistanceForScreen(final double distance){
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+        return df.format(distance);
     }
 
     @Override
@@ -32,9 +41,9 @@ public class IBeaconArrayAdapter extends ArrayAdapter {
         TextView deviceAddress = (TextView) rowView.findViewById(R.id.device_address);
         TextView deviceDistance = (TextView) rowView.findViewById(R.id.distance);
         Beacon beacon = (Beacon) beacons.get(position);
-        deviceName.setText(beacon.getBluetoothName());
+        deviceName.setText(beacon.getBluetoothName().equals("") ? UNKNOWN_DEVICE : beacon.getBluetoothName());
         deviceAddress.setText(beacon.getBluetoothAddress());
-        deviceDistance.setText(String.valueOf(beacon.getDistance()));
+        deviceDistance.setText(formatDistanceForScreen(beacon.getDistance()));
         return rowView;
     }
 }
