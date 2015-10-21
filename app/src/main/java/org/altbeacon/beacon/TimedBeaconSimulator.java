77053c26ad93f342eba2e1fc22de7fb20d2614ec
atteheino <atte.heino@gmail.com>
2015-10-21 21:32:23 +0300

    More details for the simulated Beacons.

--- app/src/main/java/org/altbeacon/beacon/TimedBeaconSimulator.java
+++ app/src/main/java/org/altbeacon/beacon/TimedBeaconSimulator.java
@@ -48,13 +48,17 @@ public class TimedBeaconSimulator implements org.altbeacon.beacon.simulator.Beac
     public void createBasicSimulatedBeacons(){
         if (USE_SIMULATED_BEACONS) {
             Beacon beacon1 = new AltBeacon.Builder().setId1("DF7E1C79-43E9-44FF-886F-1D1F7DA6997A")
-                    .setId2("1").setId3("1").setRssi(-55).setTxPower(-55).build();
+                    .setId2("1").setId3("1").setRssi(-55).setTxPower(-55)
+                    .setBluetoothAddress("60 d0 a9 ca 3b c3").build();
             Beacon beacon2 = new AltBeacon.Builder().setId1("DF7E1C79-43E9-44FF-886F-1D1F7DA6997A")
-                    .setId2("1").setId3("2").setRssi(-55).setTxPower(-55).build();
+                    .setId2("1").setId3("2").setRssi(-55).setTxPower(-54)
+                    .setBluetoothAddress("60 d0 a9 ca 3b c2").build();
             Beacon beacon3 = new AltBeacon.Builder().setId1("DF7E1C79-43E9-44FF-886F-1D1F7DA6997A")
-                    .setId2("1").setId3("3").setRssi(-55).setTxPower(-55).build();
+                    .setId2("1").setId3("3").setRssi(-55).setTxPower(-53)
+                    .setBluetoothAddress("60 d0 a9 ca 3b c1").build();
             Beacon beacon4 = new AltBeacon.Builder().setId1("DF7E1C79-43E9-44FF-886F-1D1F7DA6997A")
-                    .setId2("1").setId3("4").setRssi(-55).setTxPower(-55).build();
+                    .setId2("1").setId3("4").setRssi(-55).setTxPower(-52)
+                    .setBluetoothAddress("60 d0 a9 ca 3b c5").build();
             beacons.add(beacon1);
             beacons.add(beacon2);
             beacons.add(beacon3);
@@ -71,13 +75,17 @@ public class TimedBeaconSimulator implements org.altbeacon.beacon.simulator.Beac
         if (USE_SIMULATED_BEACONS){
             beacons = new ArrayList<Beacon>();
             Beacon beacon1 = new AltBeacon.Builder().setId1("DF7E1C79-43E9-44FF-886F-1D1F7DA6997A")
-                    .setId2("1").setId3("1").setRssi(-55).setTxPower(-55).build();
+                    .setId2("1").setId3("1").setRssi(-55).setTxPower(-55)
+                    .setBluetoothAddress("60 d0 a9 ca 3b c3").build();
             Beacon beacon2 = new AltBeacon.Builder().setId1("DF7E1C79-43E9-44FF-886F-1D1F7DA6997A")
-                    .setId2("1").setId3("2").setRssi(-55).setTxPower(-55).build();
+                    .setId2("1").setId3("2").setRssi(-55).setTxPower(-54)
+                    .setBluetoothAddress("60 d0 a9 ca 3b c2").build();
             Beacon beacon3 = new AltBeacon.Builder().setId1("DF7E1C79-43E9-44FF-886F-1D1F7DA6997A")
-                    .setId2("1").setId3("3").setRssi(-55).setTxPower(-55).build();
+                    .setId2("1").setId3("3").setRssi(-55).setTxPower(-53)
+                    .setBluetoothAddress("60 d0 a9 ca 3b c1").build();
             Beacon beacon4 = new AltBeacon.Builder().setId1("DF7E1C79-43E9-44FF-886F-1D1F7DA6997A")
-                    .setId2("1").setId3("4").setRssi(-55).setTxPower(-55).build();
+                    .setId2("1").setId3("4").setRssi(-55).setTxPower(-52)
+                    .setBluetoothAddress("60 d0 a9 ca 3b c5").build();
             beacons.add(beacon1);
             beacons.add(beacon2);
             beacons.add(beacon3);
